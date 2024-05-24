618f2f0771b7ed60d64af0db0fae945d69edd571
lcozdenm <loic.cozdenmat@gmail.com>
2024-05-24 14:47:12 +0200

    Module06: ex02 done

---
+++ day06/ex02/Base.cpp
@@ -0,0 +1,53 @@
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+#include "random"
+#include <ctime>
+#include <iostream>
+
+Base::~Base() {}
+
+Base *generate(void) {
+
+	int res;
+
+	srand(std::time(NULL));
+	
+	res = rand() % 3;
+	switch (res)
+	{
+		case 0:
+			return (new A());
+		case 1:
+			return (new B());
+		case 2:
+			return (new C());
+	}
+	return (NULL);
+}
+
+void identify(Base *p) {
+
+	if (dynamic_cast<A *>(p) != NULL)
+		std::cout << "pointer is of type A!" << std::endl;
+	else if (dynamic_cast<B *>(p) != NULL)
+		std::cout << "pointer is of type B!" << std::endl;
+	else if (dynamic_cast<C *>(p) != NULL)
+		std::cout << "pointer is of type C !" << std::endl;
+	else
+		std::cout << "couldn't convert pointer to any type" << std::endl;
+}
+
+void identify(Base &p) {
+
+	if (dynamic_cast<A *>(&p) != NULL)
+		std::cout << "pointer is of type A!" << std::endl;
+	else if (dynamic_cast<B *>(&p) != NULL)
+		std::cout << "pointer is of type B!" << std::endl;
+	else if (dynamic_cast<C *>(&p) != NULL)
+		std::cout << "pointer is of type C !" << std::endl;
+	else
+		std::cout << "couldn't convert pointer to any type" << std::endl;
+}
